7bb977a3541778871559a59ed1f75076ac5ad4d0
hendrosetwn <hsetiawan@binar.co.id>
2024-08-16 20:04:16 +0700

    build agaaaainnn

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WidgetClient from './components/widget';
 
-// Definisikan tipe untuk props di WidgetChatbot
 interface WidgetChatbotProps {
   tenantId: string;
 }
@@ -20,17 +19,11 @@ class WidgetChatbot extends React.Component<WidgetChatbotProps> {
   }
 
   render() {
-    return <div>Hello, this is a widget for tenant: {this.props.tenantId}</div>;
+    return <WidgetClient tenantId={this.props.tenantId} />;
   }
 }
 
-// Attach ke window secara manual
-declare global {
-  interface Window {
-    WidgetChatbot: typeof WidgetChatbot;
-  }
-}
-
-window.WidgetChatbot = WidgetChatbot;
+// Attach to the window object manually
+(window as any).WidgetChatbot = WidgetChatbot;
 
 export default WidgetChatbot;
